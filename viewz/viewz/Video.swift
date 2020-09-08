403a23cb012d9fe7405844a6d9f91cd3d0a05dd6
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-08 11:23:21 -1000

    Created Constants file that will not be added to GitHub. Added Video Struct.

---
+++ viewz/viewz/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  viewz
+//
+//  Created by Jesse Ruiz on 9/8/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
