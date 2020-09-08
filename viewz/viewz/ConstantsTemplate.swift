64914767c1d5464ba944158721740e946ac8554a
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-08 11:27:04 -1000

    Added Constants Template so people can add in their own apiKey and playlistID.

---
+++ viewz/viewz/ConstantsTemplate.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsTemplate.swift
+//  viewz
+//
+//  Created by Jesse Ruiz on 9/8/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+/*
+ Need to add your own apiKey and playlistID
+ */
+
+import Foundation
+
+//struct Constants {
+//
+//    static var apiKey = ""
+//    static var playlistID = ""
+//    static var apiURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.playlistID)&key=\(Constants.apiKey)"
+//}
